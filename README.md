0399bdfbe8db763de3cf8fb918fa673d39cd9a21
james <jcotab@gmail.com>
2021-07-08 13:02:38 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# This is a read me file. . .
+Guessing Game is a simple command line application built with Rust.
+
+- Install Instructions
+    - clone repo
+    - cargo run, to run application
+    - cargo build to build application
+    - follow prompts
+    - type quit to exit
--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,35 @@ use std::cmp::Ordering;
 fn main() {
 
     println!("Guess the number!");
-    
+
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
-    
-    let mut guess = String::new();
+    loop {
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        println!("Please input your guess.");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too Large!"),
-        Ordering::Equal => println!("You Win!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Large!"),
+            Ordering::Equal => { 
+                println!("You Win!");
+                break;
+            }
+        }
     }
 }
